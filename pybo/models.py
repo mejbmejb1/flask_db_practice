d30174253001b84376bc6b42568c8689147d0574
mejbmejb <mejbmejb@naver.com>
2026-07-28 12:30:08 +0900

    추천기능을 넣기위해 db 수정     - 원글 및 답글에 추천 내용 담는 voter 필드 추가

--- pybo/models.py
+++ pybo/models.py
@@ -1,13 +1,28 @@
-from pybo import db
-
 '''
 models.py
     - 관련 DB 모델을 생성하고 관리하는 곳
     - ondelete='CASCADE': 질문이 삭제되었을 때 그에 달린 답변들도 함께 삭제되도록 한 설정은 데이터베이스의 무결성(데이터의 일관성)을 지키는 데 필수
 
 '''
+from pybo import db
+from sqlalchemy import Table
 
 max_string = 200
+
+# 중간 테이블 정의
+question_voter = Table(
+    'question_voter',
+    db.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), primary_key=True)
+)
+
+answer_voter = Table(
+    'answer_voter',
+    db.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('answer_id', db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), primary_key=True)
+)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +32,9 @@ class Question(db.Model):
     # 글쓴이 외래키 및 관계 설정 추가
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('question_set'))
+    # 추천인 (多대多)
+    voter = db.relationship('User', secondary=question_voter,
+                            backref=db.backref('question_voter_set', lazy='dynamic'))
 
     # 쉘이나 로그에서 객체를 보기 쉽게 출력해주는 메서드
     def __repr__(self):
@@ -34,6 +52,8 @@ class Answer(db.Model):
     create_date = db.Column(db.DateTime(), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('answer_set'))
+    voter = db.relationship('User', secondary=answer_voter,
+                            backref=db.backref('answer_voter_set', lazy='dynamic'))
 
     def __repr__(self):
         return f'<Answer to Question {self.question_id}>'
