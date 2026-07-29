1eecdeb245ad65f8856fccf928112727f506b995
mejbmejb <mejbmejb@naver.com>
2026-07-29 12:04:32 +0900

    답변/댓글에 마크다운을 쓸수있는 기능추가

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from pybo.filter import format_datetime
 
+import markdown 
+from markupsafe import Markup
+
 import config
 
 # DB 생성자
@@ -29,7 +32,22 @@ def create_app(): #applictaion factory 함수
     app.register_blueprint(auth_views.bp)
     app.register_blueprint(comment_views.bp)
 
+    def format_markdown(text):
+        if not text:
+            return ""
+        
+        # markdown.markdown()의 결과(문자열)를 MarkupSafe의 Markup 객체로 감싸줍니다.
+        # 이렇게 감싸주어야 템플릿(HTML)에서 꺾쇠 태그가 무력화되지 않고 화면에 잘 나옵니다.
+        # nl2br         : 줄바꿈을 <br>로 변환
+        # fenced_code   : 코드 블록 지원, <pre><code>로 변환
+        # sane_lists    : 번호 목록 작성 시 오동작 방지
+        # tables        : 표(Table) 구문 지원
+        # toc           : 제목(Header) ID 생성 및 목차 지원 
+        html_content = markdown.markdown(text, extensions=['nl2br', 'fenced_code', 'sane_lists', 'tables', 'toc'])
+        return Markup(html_content)
+
     # jinja_env 필터에 등록
     app.jinja_env.filters['datetime'] = format_datetime
+    app.jinja_env.filters['markdown'] = format_markdown
 
     return app
